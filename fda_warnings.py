dd813ec85ca9662d7e1e043420044f8839286a55
Alex Church <35304852+alexchurch29@users.noreply.github.com>
2018-10-21 01:01:44 -0400

    Add files via upload

--- fda_warnings.py
+++ fda_warnings.py
@@ -3,22 +3,43 @@ Used to scrape team FDA warnings
 """
 
 from bs4 import BeautifulSoup
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 import time
 import pandas as pd
-import numpy as np
 import re
-from helper import get_url
 
-url_key = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-              'W', 'X', 'Y', 'Z', '0-9']
+url_key = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+           'X', 'Y', 'Z', '0-9']
+
+
+def get_url(url):
+    """
+    Get the url
+    :param url: given url
+    :return: raw html
+    """
+    response = requests.Session()
+    retries = Retry(total=10, backoff_factor=.1)
+    response.mount('http://', HTTPAdapter(max_retries=retries))
+
+    try:
+        response = response.get(url, timeout=5)
+        response.raise_for_status()
+    except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError):
+        return None
+
+    return response
 
 
 def scrape_data(key):
     """
-    Scrapes the FDA website to compile a list of companies who have had a warning issued against them
-    :return: an array of arrays from the given key page with all html tags stripped from the text
+    Parses html from the FDA website to compile a list of companies who have had an FDA warning issued against them
+    :return: an array of company and warning info
     """
 
+    # obtain the full html text
     try:
         html = get_url('https://www.accessdata.fda.gov/scripts/warningletters/wlSearchResult.cfm?firstChar={}'.format(key))
         time.sleep(1)
@@ -31,11 +52,12 @@ def scrape_data(key):
     arr = [x.get_text() for x in table]
 
     # the below code is used to strip unnecessary html tags
-    arr = [re.split('\xa0|\n', i) for i in arr]
-    for i,j in reversed(list(enumerate(arr))):
-        for k,l in reversed(list(enumerate(arr[i]))):
+    arr = [re.split('\xa0', i) for i in arr]
+    for i, j in reversed(list(enumerate(arr))):
+        for k, l in reversed(list(enumerate(arr[i]))):
             arr[i][k] = arr[i][k].replace('\t', '')
-            arr[i][k] = arr[i][k].replace('\r', '')
+            arr[i][k] = arr[i][k].replace('\r\n', ' ')
+            arr[i][k] = arr[i][k].strip()
             if arr[i][k] == '':
                 arr[i].pop(k)
 
@@ -44,19 +66,70 @@ def scrape_data(key):
     del arr[-1]
 
     # split by row
-    arr = [arr[i:i + 6] for i in range(0, len(arr),6)]
+    arr = [arr[i:i + 6] for i in range(0, len(arr), 6)]
 
-    for i,j in reversed(list(enumerate(arr))):
-        for k,l in reversed(list(enumerate(arr[i]))):
+    for i, j in reversed(list(enumerate(arr))):
+        for k, l in reversed(list(enumerate(arr[i]))):
             if k == 3:
                 arr[i][k] = [" - ".join(arr[i][k])]
+
+    # add the warning report urls
+    soup = BeautifulSoup(html.content, 'html.parser')
+    table = soup.find_all('a', href=True)
+
+    warnings = []
+    for x in table:
+        warnings.append(str(x).replace("&amp;", "&"))
+    for i, j in enumerate(list(warnings)):
+        warnings[i] = warnings[i].split("title=")
+
+    ind = warnings.index(['<a href="wlAdvancedSearch.cfm" ', '"Advanced Search">Advanced Search</a>']) + 1
+    for i, j in enumerate(list(arr)):
+        arr[i].append([warnings[ind][0][9:warnings[ind][0].rindex('"')]])
+
+        '''
+        # extract contact info from warning reports
+        try:
+            html = get_url(warnings[ind][0][9:warnings[ind][0].rindex('"')])
+            time.sleep(1)
+        except Exception as e:
+            raise Exception
+
+        soup = BeautifulSoup(html.content, 'html.parser')
+        table = soup.findAll("div")
+
+        arr = [x.get_text(separator='div') for x in table]
+        arr = [re.split('\xa0|\n|div', i) for i in arr]
+
+        for m, n in reversed(list(enumerate(arr))):
+            for o, p in reversed(list(enumerate(arr[m]))):
+                arr[m][o] = arr[m][o].strip()
+                if arr[m][o] == '' or arr[m][o] == ' ':
+                    arr[m].pop(o)
+
+        contact = ""
+        ind2 = arr[1].index('WARNING LETTER') + 2
+        print(arr)
+        while len(arr[1][ind2]) < 4 or arr[1][ind2][0:4] != 'Dear':
+            contact += arr[1][ind2] + " "
+            ind2 += 1
+
+        # contact_list = []
+        # i = re.search('\d', contact)
+        # contact_list.append(contact[0:i.start()].strip())
+        # contact_list.append(contact[i.start():len(contact) + 1].strip())
+
+        arr[i].append([contact])
+        '''
+
+        ind += 1
 
     return arr
 
 
 def parse_data():
     """
-    create dataframe with all warnings issued by the FDA that are on record
+    create dataframe with all warnings issued by the FDA that are on record, along with associated company info
     :return: dataframe with all records
     """
     arr = []
@@ -66,40 +139,13 @@ def parse_data():
             arr.append(i)
 
     headers = ['LEGAL_NAME', 'WARNING_ISSUED_DATE', 'ISSUING_OFFICE', 'DETAILS', 'RESPONSE_LETTER_POSTED',
-               'CLOSEOUT DATE']
+               'CLOSEOUT DATE', 'WARNING_LETTER']
     df = pd.DataFrame(arr, columns=headers)
-    df['WARNING_LETTER'] = np.NaN
     # covert data from list to str for formatting
     for header in headers:
         df[header] = df[header].str[0]
-    # df.to_excel("FDA_warnings.xlsx", index=False)
+    df.to_excel("FDA_warnings.xlsx", index=False)
 
     return df
 
-# print(parse_data())
-
-try:
-    html = get_url('https://www.accessdata.fda.gov/scripts/warningletters/wlSearchResult.cfm?firstChar={}'.format('A'))
-    time.sleep(1)
-except Exception as e:
-    raise Exception
-
-soup = BeautifulSoup(html.content, 'html.parser')
-table = soup.find_all('a', href=True)
-
-paragraphs = []
-for x in table:
-    paragraphs.append(str(x).replace("&amp;", "&"))
-for i, j in enumerate(list(paragraphs)):
-    paragraphs[i] = paragraphs[i].split("title=")
-
-print(paragraphs)
-
-df = parse_data()
-
-for i in df['LEGAL_NAME'].values:
-    for j in paragraphs:
-        if len(j) > 1 and j[0][1:9] == 'a href="' and j[1][1:j[1].rindex('"')] == df.loc[[i]['LEGAL_NAME']]:
-            df.loc[[i]['WARNING_LETTER']] = j[0][9:j[0].rindex('"')]
-
-print(df)
+print(parse_data())
