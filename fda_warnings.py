cd3dd11fda529d1a683fc4f3d9b7094be2a900fb
Alex Church <35304852+alexchurch29@users.noreply.github.com>
2018-10-21 11:43:06 -0400

    Add files via upload

--- fda_warnings.py
+++ fda_warnings.py
@@ -1,5 +1,7 @@
 """
-Used to scrape team FDA warnings
+Used to scrape together a list of FDA warnings issued over the previous 6 years
+The purpose is to flag food exporters to Canada if they have previously been deemed violators in other jurisdictions
+e.g. https://www.accessdata.fda.gov/scripts/warningletters/wlSearchResult.cfm?firstChar=A
 """
 
 from bs4 import BeautifulSoup
@@ -14,9 +16,14 @@ url_key = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O',
            'X', 'Y', 'Z', '0-9']
 
 
+def main():
+    parse_data()
+    return
+
+
 def get_url(url):
     """
-    Get the url
+    Get the html for a given url
     :param url: given url
     :return: raw html
     """
@@ -36,7 +43,8 @@ def get_url(url):
 def scrape_data(key):
     """
     Parses html from the FDA website to compile a list of companies who have had an FDA warning issued against them
-    :return: an array of company and warning info
+    :param: alphabetical key for the desired search page url
+    :return: a list of all warnings issued by the fda for companies starting with the given alphabetical key
     """
 
     # obtain the full html text
@@ -51,7 +59,7 @@ def scrape_data(key):
 
     arr = [x.get_text() for x in table]
 
-    # the below code is used to strip unnecessary html tags
+    # the below code is used to strip any remaining html tags
     arr = [re.split('\xa0', i) for i in arr]
     for i, j in reversed(list(enumerate(arr))):
         for k, l in reversed(list(enumerate(arr[i]))):
@@ -61,13 +69,14 @@ def scrape_data(key):
             if arr[i][k] == '':
                 arr[i].pop(k)
 
-    # remove headers and footer
+    # remove headers and footer for convenience
     del arr[0:14]
     del arr[-1]
 
     # split by row
     arr = [arr[i:i + 6] for i in range(0, len(arr), 6)]
 
+    # join the subject array into one string
     for i, j in reversed(list(enumerate(arr))):
         for k, l in reversed(list(enumerate(arr[i]))):
             if k == 3:
@@ -87,8 +96,8 @@ def scrape_data(key):
     for i, j in enumerate(list(arr)):
         arr[i].append([warnings[ind][0][9:warnings[ind][0].rindex('"')]])
 
+        # failed attempt to extract contact info from warning reports
         '''
-        # extract contact info from warning reports
         try:
             html = get_url(warnings[ind][0][9:warnings[ind][0].rindex('"')])
             time.sleep(1)
@@ -98,28 +107,40 @@ def scrape_data(key):
         soup = BeautifulSoup(html.content, 'html.parser')
         table = soup.findAll("div")
 
-        arr = [x.get_text(separator='div') for x in table]
-        arr = [re.split('\xa0|\n|div', i) for i in arr]
+        arr2 = [x.get_text(separator='div') for x in table]
+        arr2 = [re.split('\xa0|div', i) for i in arr2]
 
-        for m, n in reversed(list(enumerate(arr))):
-            for o, p in reversed(list(enumerate(arr[m]))):
-                arr[m][o] = arr[m][o].strip()
-                if arr[m][o] == '' or arr[m][o] == ' ':
-                    arr[m].pop(o)
+        for m, n in reversed(list(enumerate(arr2))):
+            for o, p in reversed(list(enumerate(arr2[m]))):
+                arr2[m][o] = arr2[m][o].replace('\t', '')
+                arr2[m][o] = arr2[m][o].replace('\r\n', ' ')
+                arr2[m][o] = arr2[m][o].strip()
+                if arr2[m][o] == '' or arr2[m][o] == ' ':
+                    arr2[m].pop(o)
 
         contact = ""
-        ind2 = arr[1].index('WARNING LETTER') + 2
-        print(arr)
-        while len(arr[1][ind2]) < 4 or arr[1][ind2][0:4] != 'Dear':
-            contact += arr[1][ind2] + " "
-            ind2 += 1
+        address = ""
 
-        # contact_list = []
-        # i = re.search('\d', contact)
-        # contact_list.append(contact[0:i.start()].strip())
-        # contact_list.append(contact[i.start():len(contact) + 1].strip())
+        try:
+            marker = arr2[1].index(arr[i][0])
+            while re.search('\d', arr2[1][marker - 1]) is None:
+                contact += arr2[1][marker - 1] + " "
+                marker -= 1
+            contact = contact.strip()
 
-        arr[i].append([contact])
+            marker = arr2[1].index(arr[i][0])
+            while re.search(r'.*(\d{5}(\-\d{4})?)$', arr2[1][marker + 1]) is None:
+                address += arr2[1][marker + 1] + " "
+                marker += 1
+            address += arr2[1][marker + 1]
+            address = address.strip()
+            arr[i].append([contact])
+            arr[i].append([address])
+        except Exception as e:
+            print("not able to parse contact/address info for {}".format(warnings[ind][0][9:warnings[ind][0].rindex('"')]))
+            arr[i].append([""])
+            arr[i].append([""])
+            pass
         '''
 
         ind += 1
@@ -132,20 +153,24 @@ def parse_data():
     create dataframe with all warnings issued by the FDA that are on record, along with associated company info
     :return: dataframe with all records
     """
+
+    # loop through the full alphabetical index to compile a list of all warnings
     arr = []
     for key in url_key:
         page = scrape_data(key)
         for i in page:
             arr.append(i)
 
+    # create the dataframe
     headers = ['LEGAL_NAME', 'WARNING_ISSUED_DATE', 'ISSUING_OFFICE', 'DETAILS', 'RESPONSE_LETTER_POSTED',
                'CLOSEOUT DATE', 'WARNING_LETTER']
     df = pd.DataFrame(arr, columns=headers)
-    # covert data from list to str for formatting
+    # convert data from list to str for formatting purposes
     for header in headers:
         df[header] = df[header].str[0]
     df.to_excel("FDA_warnings.xlsx", index=False)
 
     return df
 
-print(parse_data())
+if __name__ == '__main__':
+    main()
